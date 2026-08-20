689a92bd77522717373598b55256eb240d18e051
Shubham Tiwari <tiwarishubham1718@gmail.com>
2026-08-20 16:21:53 +0530

    Implement longest consecutive sequence function

---
+++ Array/longestConsecutiveSquence.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int longestConsecutive(int[] nums) {
+        if(nums.length==0) return 0;
+        HashSet<Integer> set=new HashSet<>();
+
+        for(int num:nums){
+            set.add(num);
+        }
+        int longest=1;
+        for(int num:set){
+            if(!set.contains(num-1)){
+                int currentNum=num;
+                int currentStreak=1;
+                
+                while(set.contains(currentNum+1)){
+                    currentNum+=1;
+                    currentStreak+=1;
+                }
+                
+                longest=Math.max(longest,currentStreak);
+            }
+        }
+        return longest;
+    }    
+}
